b0651135fa166471b0b706898578d9c88bb16e7d
tzr897 <ziruitang897@gmail.com>
2022-08-12 22:55:02 -0700

    test qps

--- main.cpp
+++ main.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
 #include "node.h"
+
+#include <time.h>
+#include <stdlib.h>
+#include <unistd.h>
+
 #include "skiplist.h"
 
 #define STORE_FILE "store/dumpFile"
@@ -8,35 +13,33 @@ Skiplist<int, string> test(0);
 
 
 int main() {
+    int times = 10000000;
+    auto begin_t = std::chrono::system_clock::now();
+    Skiplist<int, string> sl(20);
+    for (int i = 0; i < times; ++i) {
+        int write_idx = rand() % 100000;
+        sl.insert_element(write_idx, "write");
+        // int read_idx = rand() % 100000;
+        // sl.search_element(read_idx);
+    }
+    auto end_t = std::chrono::system_clock::now();
+    std::chrono::duration<double> diff = end_t - begin_t;
+    double d = diff.count();
+    double write_speed = double(times) / d;
     
-    Skiplist<int, string> sl(10);
-    sl.insert_element(1, "one");
-    sl.insert_element(3, "last");
-    sl.insert_element(5, "kiss");
-    sl.insert_element(6, "beautiful");
-    sl.insert_element(8, "world");
-    sl.insert_element(10, "fly");
-    sl.insert_element(17, "me");
-    sl.insert_element(20, "to");
-    sl.insert_element(21, "the");
-    sl.insert_element(56, "moon");
-    sl.insert_element(33, "eva");
-    sl.insert_element(33, "ave");
-
-    cout << "skiplist size is: " << sl.get_size() << endl;
-
-    sl.search_element(10);
-    sl.search_element(11);
-    
-    sl.display_list();
-
-    sl.delete_element(56);
-    sl.delete_element(3);
-    sl.delete_element(34);
-
-    cout << "skiplist size is: " << sl.get_size() << endl;
-
-    sl.display_list();
-
+//////////////////////////////////////////////////////////////////
+    begin_t = std::chrono::system_clock::now();
+    for (int i = 0; i < times; ++i) {
+        // int write_idx = rand() % 100000;
+        // sl.insert_element(write_idx, "write");
+        int read_idx = rand() % 100000;
+        sl.search_element(read_idx);
+    }
+    end_t = std::chrono::system_clock::now();
+    diff = end_t - begin_t;
+    d = diff.count();
+    double read_speed = double(times) / d;
+    cout << "write: " << write_speed << endl;
+    cout << "read: " << read_speed << endl;
     return 0;
 }
